7755fa74fd3e1ab8339da4afac2d9648e89af005
Mee Seong Im <MeeSeongIm@users.noreply.github.com>
2014-10-29 23:12:34 -0500

    Create queue_ex.py

---
+++ queue_ex.py
@@ -0,0 +1,27 @@
+
+# Deque: a doubly linked list. List: an array. 
+# The main advantage of a deque: insertions and deletions at the beginning. 
+# The main advantage of a list: fast random reads.  
+
+from collections import deque  
+n = 20
+
+queue = deque([])
+ 
+for i in list(range(n)):
+    queue.append(i)    
+print(queue)
+
+queue.extend(("break", "break"))
+
+for i in list(range(n+1,2*n+1)):
+    queue.append(i)
+print(queue)
+
+for i in list(range(int(n/2))):
+    queue.popleft()
+
+print(queue)
+
+
+
